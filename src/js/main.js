40240b665ef3ab307a7c26c6b2295af3651a48a0
Roma Kulik <kulikr.js@mail.ru>
2022-10-24 13:51:34 +0300

    completed 3, 4 item (form)

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 import './slider';
 import modals from "./modules/modals";
 import tabs from "./modules/tabs";
+import forms from "./modules/forms";
 
 // --------- Modal -----------
 
 document.addEventListener('DOMContentLoaded', () => {
+  'use strict'
 
   modals();
   tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'after_click');
   tabs('.decoration_slider', '.no_click', '.decor_cont', 'after_click');
+  forms();
 
 });
